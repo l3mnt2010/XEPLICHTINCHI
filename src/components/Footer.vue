f6919efe4a6df4b3a5fe16bca6054b0b3ef475f6
llamtruongdev365 <126931874+llamtruongdev365@users.noreply.github.com>
2024-06-25 02:09:18 +0700

    feat: add video

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,7 @@
 <template>
     <footer class="text-center text-white text-sm">
         <span class="text-white font-bold">Nguồn: ngosangns</span>
-        <span class="text-white font-bold">Clone ké by l3mnt2010 :v</span>
-         <button class="text-white font-bold bg-green-600 rounded-md" onclick="window.print()">Lưu về PDF</button>
+        <span class="text-white font-bold">Clone ké by l3mnt2010 :v</span> <span><button class="text-white font-bold bg-green-600 rounded-md" onclick="window.print()">Lưu về PDF</button></span>
     </footer>
 </template>
 
