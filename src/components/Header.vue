1031c1a6603448e1d76517c33cda3fa52e71fa38
llamtruongdev365 <126931874+llamtruongdev365@users.noreply.github.com>
2024-06-25 01:40:23 +0700

    feat: add video

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,21 +7,6 @@
         <h1 class="text-3xl">
             <a href="/">HỖ TRỢ XẾP LỊCH TÍN CHỈ KMA FROM KIT CLONE BY l3mnt2010</a>
         </h1>
-        <p class="inline-flex gap-2 mt-2">
-            <a
-                href=""
-                target="_blank"
-                rel="noreferrer"
-                class="underline"
-            >
-                KIT CLUB
-                <img
-                    :src="externalLinkIcon"
-                    class="inline-block w-3 h-3 mb-4"
-                    alt=""
-                />
-            </a>
-        </p>
     </header>
 </template>
 
